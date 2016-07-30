e2a3735d25f034d229feb0d29d91d987adb00db8
jacobplaster <j.plaster@2013.hull.ac.uk>
2016-07-30 23:52:39 +0100

    updated exported sql file

--- mysql/base-db.sql
+++ mysql/base-db.sql
@@ -1,20 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.3
--- https://www.phpmyadmin.net/
+-- version 4.4.10
+-- http://www.phpmyadmin.net
 --
--- Host: db
--- Generation Time: Jul 27, 2016 at 12:23 AM
--- Server version: 5.7.13
--- PHP Version: 5.6.23
+-- Host: localhost:3306
+-- Generation Time: Jul 31, 2016 at 12:51 AM
+-- Server version: 5.5.42
+-- PHP Version: 5.4.42
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `learnx`
@@ -28,25 +22,26 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `stream_chat` (
   `user_id` int(11) NOT NULL,
-  `tag` varchar(32) NOT NULL
+  `tag` varchar(32) NOT NULL,
+  `state` int(11) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `stream_chat`
 --
 
-INSERT INTO `stream_chat` (`user_id`, `tag`) VALUES
-(27, 'cssfg'),
-(29, 'sdsfdft'),
-(30, 'ddgfhaadyerxg234'),
-(18, '213dttvv'),
-(18, 'myfirststream'),
-(28, 'leagueoflegends'),
-(22, 'grad_cam_1'),
-(19, 'cam_2'),
-(24, 'cam_4'),
-(25, 'dfgdgfh'),
-(20, '32afvdf4');
+INSERT INTO `stream_chat` (`user_id`, `tag`, `state`) VALUES
+(27, 'cssfg', 0),
+(29, 'sdsfdft', 0),
+(30, 'ddgfhaadyerxg234', 0),
+(18, '213dttvv', 0),
+(18, 'myfirststream', 0),
+(28, 'leagueoflegends', 0),
+(22, 'grad_cam_1', 0),
+(19, 'cam_2', 0),
+(24, 'cam_4', 0),
+(25, 'dfgdgfh', 0),
+(20, '32afvdf4', 0);
 
 -- --------------------------------------------------------
 
@@ -59,6 +54,7 @@ CREATE TABLE `stream_video` (
   `title` varchar(32) NOT NULL,
   `tag` varchar(32) NOT NULL,
   `description` varchar(400) NOT NULL,
+  `state` int(11) NOT NULL DEFAULT '0',
   `stream_key` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -66,18 +62,18 @@ CREATE TABLE `stream_video` (
 -- Dumping data for table `stream_video`
 --
 
-INSERT INTO `stream_video` (`user_id`, `title`, `tag`, `description`, `stream_key`) VALUES
-(27, 'Building a custom pc', 'cssfg', 'It has survived not only five centuries, but also the leap into electronic typesetting.', 'r6pxxh6!gn'),
-(29, 'Live with countryfile mag', 'sdsfdft', 'Using new stream to reach out to my fans!!!! Hellooo.', 'qwxgtalqe0'),
-(30, 'Live with PC mag', 'ddgfhaadyerxg234', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s', '9gpz-8t7tt'),
-(18, 'Live with countryfile mag', '213dttvv', 'It has survived not only five centuries, but also the leap into electronic typesetting.', '0ah889wymx'),
-(18, 'Live with PC mag', 'myfirststream', 'when an unknown printer took a galley of type and scrambled it to make a type specimen book.', 'yqz9az2hz?'),
-(28, 'Graduation - Hull University', 'leagueoflegends', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s', 'hnockoq6bx'),
-(22, 'League of legends - TSM', 'grad_cam_1', 'when an unknown printer took a galley of type and scrambled it to make a type specimen book.', 'g0ppbh5jvg'),
-(19, 'Cooking time - roast dinner', 'cam_2', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s', 'd-lbc8kqvo'),
-(24, 'Live with PC mag', 'cam_4', 'Using new stream to reach out to my fans!!!! Hellooo.', 'd77zb74?tp'),
-(25, 'Graduation - London University', 'dfgdgfh', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s', 'xwfzk7o5a-'),
-(20, 'Cooking time - roast dinner', '32afvdf4', 'Using new stream to reach out to my fans!!!! Hellooo.', 'u48ox3ilfj');
+INSERT INTO `stream_video` (`user_id`, `title`, `tag`, `description`, `state`, `stream_key`) VALUES
+(27, 'Building a custom pc', 'cssfg', 'It has survived not only five centuries, but also the leap into electronic typesetting.', 0, 'r6pxxh6!gn'),
+(29, 'Live with countryfile mag', 'sdsfdft', 'Using new stream to reach out to my fans!!!! Hellooo.', 0, 'qwxgtalqe0'),
+(30, 'Live with PC mag', 'ddgfhaadyerxg234', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s', 0, '9gpz-8t7tt'),
+(18, 'Live with countryfile mag', '213dttvv', 'It has survived not only five centuries, but also the leap into electronic typesetting.', 0, '0ah889wymx'),
+(18, 'Live with PC mag', 'myfirststream', 'when an unknown printer took a galley of type and scrambled it to make a type specimen book.', 0, 'yqz9az2hz?'),
+(28, 'Graduation - Hull University', 'leagueoflegends', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s', 0, 'hnockoq6bx'),
+(22, 'League of legends - TSM', 'grad_cam_1', 'when an unknown printer took a galley of type and scrambled it to make a type specimen book.', 0, 'g0ppbh5jvg'),
+(19, 'Cooking time - roast dinner', 'cam_2', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s', 0, 'd-lbc8kqvo'),
+(24, 'Live with PC mag', 'cam_4', 'Using new stream to reach out to my fans!!!! Hellooo.', 0, 'd77zb74?tp'),
+(25, 'Graduation - London University', 'dfgdgfh', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s', 0, 'xwfzk7o5a-'),
+(20, 'Cooking time - roast dinner', '32afvdf4', 'Using new stream to reach out to my fans!!!! Hellooo.', 0, 'u48ox3ilfj');
 
 -- --------------------------------------------------------
 
@@ -90,7 +86,7 @@ CREATE TABLE `users` (
   `username` varchar(24) NOT NULL,
   `password` varchar(64) NOT NULL,
   `email` varchar(64) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
@@ -132,7 +128,4 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=33;
