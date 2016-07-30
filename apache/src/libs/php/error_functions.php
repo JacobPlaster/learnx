1987f761b03a43e3b010be01a6a7d01571508f8b
Jacob Plaster <J.Plaster@2013.hull.ac.uk>
2016-07-30 17:43:09 +0100

    updated files to work with new database structure

--- apache/src/libs/php/error_functions.php
+++ apache/src/libs/php/error_functions.php
@@ -1,10 +1,12 @@
 <?php
+  require_once($_SERVER['DOCUMENT_ROOT'].'/cfg.php');
 
   function call404Error($message)
   {
-    header('HTTP/1.1 404 Not Found');
+    global $SERVER_PATH;
+    //header('HTTP/1.1 404 Not Found');
     $_GET['e'] = 404;
-    include $_SERVER['DOCUMENT_ROOT'].'/learnx-service/pages/error.php';
+    include($SERVER_PATH['inserts-error']);
     exit;
   }
 
