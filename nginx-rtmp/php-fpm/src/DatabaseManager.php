33d340428e2d16ca414a149d12aada04e7f1bf23
jacobplaster <j.plaster@2013.hull.ac.uk>
2016-10-14 23:50:48 +0100

    added more sql interaction with rtmp origin

--- nginx-rtmp/php-fpm/src/DatabaseManager.php
+++ nginx-rtmp/php-fpm/src/DatabaseManager.php
@@ -50,12 +50,125 @@ class DatabaseManager {
 
     /**
      *
+     * Gets the user data by its secure key
+     *
+     * @param users username
+     *
+     */
+    function getStreamByKey($key)
+    {
+      $query_string="SELECT * FROM ".$this->STREAMS_VIDEO_TABLE." WHERE stream_key='".$key."' LIMIT 1";
+      $result=$this->conn->query($query_string)->fetch_object();
+      if(sizeof($result)>=1)
+        return $result;
+      else
+        return NULL;
+    }
+
+
+    /**
+     *
+     * Gets the user data by its id tag (app name)
+     *
+     * @param users username
+     *
+     */
+    function getStreamByTag($tag)
+    {
+      $query_string="SELECT * FROM ".$this->STREAMS_VIDEO_TABLE." WHERE tag='".$tag."' LIMIT 1";
+      $result=$this->conn->query($query_string)->fetch_object();
+      if(sizeof($result)>=1)
+        return $result;
+      else
+        return NULL;
+    }
+
+
+    /**
+     *
+     * Sets the state of the stream
+     *
+     * @param The id of the targeted user
+     * @param The state of the stream (1 = live, 0 = offline)
+     *
+     */
+    function setStreamState($id, $state)
+    {
+      $query_string="UPDATE ".$this->STREAMS_VIDEO_TABLE." SET state='".$state."' WHERE id='".$id."'";
+      $result = $this->conn->query($query_string);
+
+      if(!$result){
+        die('Error : ('. $mysqli->errno .') '. $mysqli->error);
+      }
+    }
+
+
+    /**
+     *
+     * Updates the number of connections to the stream in the database (+1)
+     *
+     * @param The id of the targeted stream
+     *
+     */
+    function incrementViewCount($id)
+    {
+      $query_string="UPDATE ".$this->STREAMS_VIDEO_TABLE." SET numOfConnections=numOfConnections+1 WHERE id='".$id."'";
+      $result = $this->conn->query($query_string);
+
+      if(!$result){
+        die('Error : ('. $mysqli->errno .') '. $mysqli->error);
+      }
+    }
+
+
+    /**
+     *
+     * Updates the number of connections to the stream in the database (-1)
+     *
+     * @param The id of the targeted stream
+     *
+     */
+    function decrementViewCount($id)
+    {
+      $query_string="UPDATE ".$this->STREAMS_VIDEO_TABLE." SET numOfConnections=numOfConnections-1 WHERE id='".$id."'";
+      $result = $this->conn->query($query_string);
+
+      if(!$result){
+        die('Error : ('. $mysqli->errno .') '. $mysqli->error);
+      }
+    }
+
+
+    // add the video that being recorded to the database
+
+
+    // get stream details
+    // SELECT stream_key, id, user_id, recordable, numOfConnections, maxConnections FROM "+STREAMS_VIDEO_TABLE+" WHERE tag='"+tag+"' LIMIT 1
+
+
+    // edge only (could connect to socket.io)
+    // increment connections
+    /*
+    query = "UPDATE "+STREAMS_VIDEO_TABLE+" SET numOfConnections=numOfConnections+1 WHERE tag='"+tag+"'";
+			else
+				query = "UPDATE "+STREAMS_VIDEO_TABLE+" SET numOfConnections=numOfConnections-1 WHERE tag='"+tag+"'";
+    */
+
+
+    /**
+     *
      * Disconnects the service from the mysqli connection
      *
      */
     function disconnect()
     {
       $this->conn->close();
+    }
+
+
+    function escape_string($string)
+    {
+      return mysqli_real_escape_string($this->conn, $string);
     }
 }
 ?>
