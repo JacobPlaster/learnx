90134a3925eeda1b17de7d1f62f53a944a52d380
jacobplaster <j.plaster@2013.hull.ac.uk>
2016-11-15 21:13:22 +0000

    added nodejs rtmp api

---
+++ nginx-rtmp/node-api/routes/routes.js
@@ -0,0 +1,16 @@
+var appRouter = function(app) {
+
+   app.get("/on_publish", function(req, res) {
+    	res.send("On publish.");
+   });
+
+   app.get("/on_play", function(req, res) {
+    	res.send("On play.");
+   });
+
+   app.get("/edge_redirect", function(req, res) {
+    	res.status(300).location('rtmp://138.68.131.239:1935/origin/'+req.query.name).end();
+   });
+
+}
+module.exports = appRouter;
