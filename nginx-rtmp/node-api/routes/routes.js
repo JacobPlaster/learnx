8c868d8697663199d9f4166e9f3a313fc64fec07
jacobplaster <j.plaster@2013.hull.ac.uk>
2016-11-22 22:38:05 +0000

    edge redirect test

--- nginx-rtmp/node-api/routes/routes.js
+++ nginx-rtmp/node-api/routes/routes.js
@@ -8,10 +8,12 @@ var appRouter = function(app) {
     	res.send("On play.");
    });
 
-   /*app.get("/edge_redirect", function(req, res) {
+   app.get("/edge_redirect", function(req, res) {
     //  res.send("test");
-      res.redirect('rtmp://138.68.131.239:1935/origin/'+req.query.name);
-   });*/
+      //res.redirect('rtmp://138.68.131.239:1935/origin/'+req.query.name);
+      console.log(req.query);
+      res.redirect('rtmp://138.68.131.239:1935/origin/test');
+   });
 
    app.get('/edge_redirect/:name', function(req , res){
         res.redirect('rtmp://138.68.131.239:1935/origin/'+req.params.name);
