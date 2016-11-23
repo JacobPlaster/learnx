c44bbd078c0835d6cd518f3adafaddb884ae0cc6
jacobplaster <j.plaster@2013.hull.ac.uk>
2016-11-23 22:02:26 +0000

    trying to pull dynamic hls

--- nginx-rtmp/node-api/routes/routes.js
+++ nginx-rtmp/node-api/routes/routes.js
@@ -8,12 +8,20 @@ var appRouter = function(app) {
     	res.send("On play.");
    });
 
-   app.get("/edge_redirect", function(req, res) {
+   app.get("/rtmp_redirect", function(req, res) {
      var streamName = req.query.name;
      // check if allowed to play
      // check if stream exists
      // find best to redirect to
      res.redirect('rtmp://138.68.131.239:1935/origin/'+streamName);
+   });
+
+   app.get("/hls_redirect", function(req, res) {
+     var streamName = req.query.name;
+     // check if allowed to play
+     // check if stream exists
+     // find best to redirect to
+     res.redirect('http://138.68.131.239/hls/'+streamName+'.m3u8');
    });
 }
 module.exports = appRouter;
