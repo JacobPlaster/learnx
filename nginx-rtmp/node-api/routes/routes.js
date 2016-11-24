c7583329ac7bad4adbeeb6a20e1e8c740b089481
jacobplaster <j.plaster@2013.hull.ac.uk>
2016-11-24 21:09:41 +0000

    Changed direction of stream to push

--- nginx-rtmp/node-api/routes/routes.js
+++ nginx-rtmp/node-api/routes/routes.js
@@ -8,21 +8,13 @@ var appRouter = function(app) {
     	res.send("On play.");
    });
 
-   app.get("/rtmp_redirect", function(req, res) {
-     var streamName = req.query.name;
-     // check if allowed to play
-     // check if stream exists
-     // find best to redirect to
-     res.redirect('rtmp://138.68.131.239:1935/origin/'+streamName);
-   });
-
-   app.get("/hls_redirect", function(req, res) {
+   app.get("/origin_to_edge", function(req, res) {
      var streamName = req.query.name;
      // check if allowed to play
      // check if stream exists
      // find best to redirect to
      console.log(req);
-     res.redirect('http://138.68.131.239/hls/'+streamName+'.m3u8');
+     res.redirect('rtmp://138.68.135.81:1935/edge/'+streamName);
    });
 }
 module.exports = appRouter;
