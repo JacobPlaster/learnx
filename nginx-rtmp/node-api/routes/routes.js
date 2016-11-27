16f53e581c8374b499c56b4deb63ddafdb3e79a5
jacobplaster <j.plaster@2013.hull.ac.uk>
2016-11-27 17:59:25 +0000

    origin test

--- nginx-rtmp/node-api/routes/routes.js
+++ nginx-rtmp/node-api/routes/routes.js
@@ -17,7 +17,7 @@ var appRouter = function(app) {
      res.redirect('rtmp://138.68.135.81:1935/edge/'+streamName);
    });
 
-   app.post('/test-page', function(req, res) {
+   app.post('/origin_to_edge', function(req, res) {
     console.log(req);
     var name = req.body.name;
     res.redirect('rtmp://138.68.135.81:1935/edge/'+streamName);
