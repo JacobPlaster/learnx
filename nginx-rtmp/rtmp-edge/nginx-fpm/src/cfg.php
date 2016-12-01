b21ac4f34d4695e9aeb8e623776463eb4d1b9d9f
jacobplaster <j.plaster@2013.hull.ac.uk>
2016-12-01 20:52:09 +0000

    Finally added hls caching and re-structured folder

--- nginx-rtmp/rtmp-edge/nginx-fpm/src/cfg.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-  // Databse
-  $SERVER_CFG['MYSQL_HOST'] = "db";;
-  $SERVER_CFG['MYSQL_PORT'] = "3306";
-  $SERVER_CFG['MYSQL_USER'] = "root";
-  $SERVER_CFG['MYSQL_PWD'] = "root";
-  $SERVER_CFG['MYSQL_DATABSE_NAME'] = "learnx";
- ?>
